440bcdf0f28f5510c371138f54b877a97b5a4e1a
Pedro Santos <zer0games2@hotmail.com>
2019-04-10 13:43:15 -0300

    Fixed Port Bug

--- Noticias/init.js
+++ Noticias/init.js
@@ -8,6 +8,6 @@ var
     res.sendFile(__dirname+'/index.html'); 
   });
   
-  app.listen(3000, function () {
+  app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
   });
