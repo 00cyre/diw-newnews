5ba074251fdbadd357c1867c552ac0ffc18063bd
Pedro Santos <zer0games2@hotmail.com>
2019-04-10 12:36:30 -0300

    Fixed Nodejs

---
+++ Noticias/init.js
@@ -0,0 +1,13 @@
+var
+  ssl = true,
+  express = require('express'),
+  app = express()
+
+
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/index.html'); 
+  });
+  
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
