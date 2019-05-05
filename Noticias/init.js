2af7c754853ef7dce4cac0b131d234b6ec583154
Pedro Santos <zer0games2@hotmail.com>
2019-05-05 04:37:01 -0300

    Made it responsive

--- Noticias/init.js
+++ Noticias/init.js
@@ -3,9 +3,9 @@ var
   express = require('express'),
   app = express()
 
-
+  app.use('/res', express.static(__dirname+ '/static_res'))
   app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/index.html'); 
+    res.sendFile(__dirname + req.path); 
   });
   
   app.listen(process.env.PORT || 3000,"0.0.0.0", function () {
